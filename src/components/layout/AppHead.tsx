88d4da19aa8f38a2b23284a1bd772490b81ab864
Ariel <arieled91@gmail.com>
2022-03-31 23:43:33 -0300

    fix app head

--- src/components/layout/AppHead.tsx
+++ src/components/layout/AppHead.tsx
@@ -3,11 +3,11 @@ import NextHead from "next/head";
 const AppHead = ({ children }: { children: string }) => (
   <NextHead>
     <title>{children}</title>
+    <meta property="og:title" content={children} />
     <meta
       property="og:description"
       content="A simple web app that displays an infinite scrolling of house data."
     />
-    <meta property="og:image" content="logo.png" />
   </NextHead>
 );
 
