f3918923de1a5d4f2ea1f0725718a9f6b76c383c
Ariel <arieled91@gmail.com>
2022-03-31 23:18:36 -0300

    cleanup code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,8 @@
-import axios from "axios";
-import axiosRetry from "axios-retry";
 import useInfiniteScroll from "@/hooks/useInfiniteScroll";
 import Layout from "../components/layout/Layout";
 import { PageWithLayout } from "@/model/app";
 import Houses from "../components/houses/Houses";
 import { fetchHouses } from "@/services/houses.service";
-
-axiosRetry(axios, { retryDelay: axiosRetry.exponentialDelay });
 
 const Page: PageWithLayout = () => {
   const {
--- src/services/houses.service.ts
+++ src/services/houses.service.ts
@@ -1,6 +1,9 @@
 import axios from "axios";
 import { HousesResponse } from "@/model/house";
 import endpoints from "@/helpers/endpoints";
+import axiosRetry from "axios-retry";
+
+axiosRetry(axios, { retryDelay: axiosRetry.exponentialDelay });
 
 export const fetchHouses = async (page: number, size = 24) => {
   const response = await axios.get<HousesResponse>(endpoints.houses, {
